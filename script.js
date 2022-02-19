8e03c5966703a41fbcd753028e7f0bb8281ea1f9
Camila Morais <Camila Nobre>
2022-02-19 16:30:31 -0300

    adicionando funções js e estilos css

---
+++ script.js
@@ -0,0 +1,29 @@
+var addSix = fn(6);
+addSix(10);
+addSix(21);
+// Função para adicionar o número 6 e somar ao número 10 e posteriormente, ao numero 21
+function fn(number) {
+    return function(six) {
+        return number + six
+    }
+}
+
+//Função duplicate que vai clonar o array
+ function duplicate(arr){
+    var arr = [1, 2, 3, 4];
+    return arr.concat(arr);
+}
+
+// Transformando uma string em objeto
+   let str = '{"name":"Erick Schimdt","age":32,"height":1.78}';
+   let obj = JSON.parse(str)
+   console.log(obj)
+
+
+
+
+
+
+
+
+
